9607aafc5cca7ad001af96a9167f02d15c59bd00
Deena Thomas <deenathomas@Seans-iMac.fios-router.home>
2021-05-18 18:18:34 -0400

    started to work on making a socket connection

--- script.py
+++ script.py
@@ -1,7 +1,21 @@
 import sys
+import socket
+
+PORT = 8000
 
 def create_connection(ip):
     print(ip)
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((ip, PORT))
+        s.listen()
+        connection, address = s.accept()
+        with connection:
+            print(address)
+            while True:
+                data = connection.recv(1024)
+                if not data:
+                    break
+                connection.sendall(data)
 
 
 def initiateAttack(ip):
@@ -10,6 +24,7 @@ def initiateAttack(ip):
 
 def main():
     ip = sys.argv[1]
+    
     initiateAttack(ip)
 
 if __name__ == "__main__":
